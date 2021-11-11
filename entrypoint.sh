ae1a3c95d712effa87c6f532f8cac486c1803c8a
Griffith88 <dobrvn@gmail.com>
2021-11-11 14:04:48 +0300

    added .gitifone, .dockerignore, rebuild images, changed docker-compose.yml

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-python3 /usr/src/adminsite/networkinfo/manage.py makemigrations --no-input
-
-python3 /usr/src/adminsite/networkinfo/manage.py migrate --no-input
-
-python3 /usr/src/adminsite/networkinfo/manage.py runserver 0.0.0.0:8000
-
---
+++ networkinfo/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
+
+gunicorn networkinfo.wsgi:application --bind 0.0.0.0:8000 --reload -w 4
